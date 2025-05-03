cecea4ae7101bf26e1659d9edf507e161a2fdf7e
codeharsh27 <code.harsh26@gmail.com>
2025-05-04 01:06:59 +0530

    Day1 Flutter

--- Flutter Project/webapp/lib/main.dart
+++ Flutter Project/webapp/lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:webapp/material_homepage.dart';
+import 'package:webapp/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MaterialHomePage());
+    return MaterialApp(home: HomePage());
   }
 }
